99e03570dc5b6239f2ba64be9f183c7378ac9ddb
Jesus Alberto A. Larez Romero <55491787+jesuslarez@users.noreply.github.com>
2021-06-24 18:54:28 +0100

    Added new Sprites and Box Colliders for Skull

--- Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/TouchController.cs
+++ Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/TouchController.cs
@@ -12,10 +12,20 @@ public class TouchController : MonoBehaviour
     private float displayTime = 0.5f;
     private Vector2[] touches = new Vector2[5];
     private RaycastHit2D hit;
+    private Sprite headerSprite;
     public Sprite noseSprite;
+    public Sprite jawSprite;
+    public Sprite zygomaticSprite;
+    public Sprite maxillaSprite;
+    public Sprite orbitSprite;
+    public Sprite frontalBoneSprite;
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private void Start()
+    {
+         headerSprite = GameObject.Find("HeaderLabel").transform.GetChild(0).GetComponent<SpriteRenderer>().sprite;
+    }
     void Update()
     {
         if (Input.touchCount > 0)
@@ -28,27 +38,15 @@ public class TouchController : MonoBehaviour
                 {
                     Ray ray = Camera.main.ScreenPointToRay(theTouch.position);
                     RaycastHit hit;
-                    
-                    //To-Do refactor if else statements
-
                     if (Physics.Raycast(ray, out hit, 100))
                     {
-                        if (hit.transform.name.Equals("NoseCollider"))
+                        if (CheckCollider(hit))
                         {
-                            GameObject.Find("HeaderLabel")
-                                .transform.GetChild(0)
-                                .GetComponent<SpriteRenderer>()
-                                .sprite = noseSprite;
+                            return;
                         }
-                        else if (hit.transform.parent.name.Equals("HeaderLabel"))
+                        if (hit.transform.parent.name.Equals("HeaderLabel"))
                         {
-                            SpriteRenderer spriteRenderer = hit.transform.GetComponent<SpriteRenderer>();
-                            Texture2D texture = spriteRenderer.sprite.texture;
-                            string name2 = texture.name;
-                            if (name2.Equals("NoseLabel"))
-                            {
-                                Application.OpenURL("https://en.wikipedia.org/wiki/Human_nose");
-                            }
+                            CheckHeaderLabel(hit);
                         }
                         else
                         {
@@ -57,6 +55,75 @@ public class TouchController : MonoBehaviour
                     }
                 }
             }
+        }
+    }
+
+    private bool CheckCollider(RaycastHit hit)
+    {
+        string colliderName = hit.transform.name;
+        if (colliderName.Equals("NoseCollider"))
+        {
+            headerSprite = noseSprite;
+            return true;
+        }
+        else if (colliderName.Equals("ZygomaticColliderRight") || colliderName.Equals("ZygomaticColliderLeft"))
+        {
+            headerSprite = zygomaticSprite;
+            return true;
+        }
+        else if (colliderName.Equals("MaxilaColliderLeft")
+                 || colliderName.Equals("MaxilaColliderRight")
+                 || colliderName.Equals("MaxilaColliderCenter"))
+        {
+            headerSprite = maxillaSprite;
+            return true;
+        }
+        else if (colliderName.Equals("Jaw"))
+        {
+            headerSprite = jawSprite;
+            return true;
+        }
+        else if (colliderName.Equals("OrbitColliderRight") || colliderName.Equals("OrbitColliderLeft"))
+        {
+            headerSprite = orbitSprite;
+            return true;
+        }
+        else if (colliderName.Equals("FrontalBoneCollider"))
+        {
+            headerSprite = frontalBoneSprite;
+            return true;
+        }
+        return false;
+    }
+
+    private static void CheckHeaderLabel(RaycastHit hit)
+    {
+        SpriteRenderer spriteRenderer = hit.transform.GetComponent<SpriteRenderer>();
+        Texture2D texture = spriteRenderer.sprite.texture;
+        string labelText = texture.name;
+        if (labelText.Equals("NoseLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Human_nose");
+        }
+        else if (labelText.Equals("ZygomaticLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Zygomatic_bone");
+        }
+        else if (labelText.Equals("MaxillaLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Maxilla");
+        }
+        else if (labelText.Equals("MandibleLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Mandible");
+        }
+        else if (labelText.Equals("OrbitLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Orbit_(anatomy)");
+        }
+        else if (labelText.Equals("FrontalBoneLabel"))
+        {
+            Application.OpenURL("https://en.wikipedia.org/wiki/Frontal_bone");
         }
     }
 }
